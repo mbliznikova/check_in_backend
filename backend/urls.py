47ead6a0b0217f3b564031812db27ad5420db7b2
Margarita Bliznikova <captainderteufel@gmail.com>
2025-03-07 16:04:37 -0800

    Add views

--- backend/urls.py
+++ backend/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("check_in/", views.check_in, name="check_in"),
     path("confirm/", views.confirm, name="confirm"),
     path("report/", views.report, name="report"),
+    path("classes/", views.classes_list, name="classes"),
+    path("students/", views.student_list, name="students"),
 ]
