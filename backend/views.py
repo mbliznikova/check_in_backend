ae97c3b6118f2aebf924a80f47bc9e1bb807bbee
Margarita Bliznikova <captainderteufel@gmail.com>
2025-03-14 15:36:58 -0700

    Filter classes based on the today's day

--- backend/views.py
+++ backend/views.py
@@ -1,11 +1,23 @@
+from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
-from .models import ClassModel, Student
+from .models import ClassModel, Student, Day, Schedule
 from .serializers import StudentSerializer, ClassModelSerializer
 
 def classes_list(request):
-    classes = ClassModel.objects.all()
+    today_name = datetime.today().strftime("%A")
+
+    today_day_object = Day.objects.filter(name=today_name).first()
+    # TODO: handle the case when there is no such objects? Empty list in response?
+    # Assume (for now) that it always be, because the table should be pre-populated with all weekdays?
+
+    scheduled_today = Schedule.objects.filter(day=today_day_object).values("class_model")
+
+    classes = ClassModel.objects.filter(id__in=scheduled_today)
+    # Should they be sorted? If yes, how? Alphabetical or based on scheduled time?
+
     serializer = ClassModelSerializer(classes, many=True)
 
     response = {
