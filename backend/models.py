924547988c475b7ba929d37215dcc9fb45db155f
Margarita Bliznikova <captainderteufel@gmail.com>
2025-03-18 11:48:47 -0700

    Add check-in confirmation view

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.utils import timezone
 
@@ -28,7 +30,7 @@ class Schedule(models.Model):
     class_time = models.TimeField()
 
     class Meta:
-        unique_together = ('class_model', 'day', 'class_time')
+        unique_together = ("class_model", "day", "class_time")
 
     def __str__(self):
         return f'{self.class_model.name} on {self.day.name} at {self.class_time}'
@@ -37,8 +39,11 @@ class Attendance(models.Model):
     id = models.AutoField(primary_key=True)
     student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
     class_id = models.ForeignKey(ClassModel, on_delete=models.CASCADE)
-    attendance_date = models.DateTimeField(default=timezone.now)
+    attendance_date = models.DateField(default=datetime.date.today)
     is_showed_up = models.BooleanField(default=True)
+
+    class Meta:
+        unique_together = ("student_id", "class_id", "attendance_date")
 
 class Payments(models.Model):
     id = models.AutoField(primary_key=True)
