538e1c3758d3cf7b456a6d33d1d1e20643b79bf2
Margarita Bliznikova <captainderteufel@gmail.com>
2025-03-26 09:36:33 -0700

    Add tests

--- backend/tests.py
+++ backend/tests.py
@@ -1,3 +1,101 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.timezone import now
+
+from .models import ClassModel, Student, Attendance
+
+class CheckInTestCase(TestCase):
+    def positive_response_helper(self, response, expected_status, message):
+        self.assertEqual(response.status_code, expected_status)
+        response_data = json.loads(response.content)
+        self.assertIn("message", response_data)
+        self.assertEqual(response_data.get("message"), message)
+
+    def setUp(self):
+        self.test_student = Student.objects.create(first_name="John", last_name="Testovich")
+        self.class_one = ClassModel.objects.create(name="Foil")
+        self.class_two = ClassModel.objects.create(name="Heavy sabre")
+
+        self.today = now().date().isoformat()
+
+        self.check_in_url = reverse("check_in")
+
+    def test_student_checks_in(self):
+        request_data = {
+            "checkInData": {
+                "studentId": self.test_student.id,
+                "classesList": [self.class_one.id],
+                "todayDate": self.today,
+            }
+        }
+
+        response = self.client.post(self.check_in_url, json.dumps(request_data), content_type="application/json")
+        self.positive_response_helper(response, 200, "Check-in data was successfully updated")
+
+        attendance_record = Attendance.objects.filter(student_id=self.test_student, attendance_date=self.today)
+        self.assertEqual(attendance_record.count(), 1)
+
+    def test_student_checks_out(self):
+        Attendance.objects.create(student_id=self.test_student, class_id=self.class_one, attendance_date=self.today)
+        attendance_initial_record = Attendance.objects.filter(student_id=self.test_student, attendance_date=self.today)
+        self.assertEqual(attendance_initial_record.count(), 1)
+
+        request_data = {
+            "checkInData": {
+                "studentId": self.test_student.id,
+                "classesList": [],
+                "todayDate": self.today,
+            }
+        }
+
+        response = self.client.post(self.check_in_url, json.dumps(request_data), content_type="application/json")
+        self.positive_response_helper(response, 200, "Check-in data was successfully updated")
+
+        attendance_final_record = Attendance.objects.filter(student_id=self.test_student, attendance_date=self.today)
+        self.assertEqual(attendance_final_record.count(), 0)
+
+    def test_student_checks_in_one_class_checks_out_another_class(self):
+        Attendance.objects.create(student_id=self.test_student, class_id=self.class_one, attendance_date=self.today)
+        attendance_initial_record = list(Attendance.objects.filter(student_id=self.test_student, attendance_date=self.today).values_list("class_id", flat=True))
+        self.assertEqual(len(attendance_initial_record), 1)
+        self.assertEqual(attendance_initial_record[0], self.class_one.id)
+
+        request_data = {
+            "checkInData": {
+                "studentId": self.test_student.id,
+                "classesList": [self.class_two.id],
+                "todayDate": self.today,
+            }
+        }
+
+        response = self.client.post(self.check_in_url, json.dumps(request_data), content_type="application/json")
+        self.positive_response_helper(response, 200, "Check-in data was successfully updated")
+
+        attendance_final_record = list(Attendance.objects.filter(student_id=self.test_student, attendance_date=self.today).values_list("class_id", flat=True))
+        self.assertEqual(len(attendance_final_record), 1)
+        self.assertEqual(attendance_final_record[0], self.class_two.id)
+
+    def test_missing_required_field(self):
+        request_data = {
+            "checkInData": {
+                "studentId": self.test_student.id,
+                "classesList": [self.class_two.id],
+            }
+        }
+
+        response = self.client.post(self.check_in_url, json.dumps(request_data), content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+
+        response_data = json.loads(response.content)
+        self.assertIn("error", response_data)
+        self.assertEqual(response_data.get("error"), "Missing required fields")
+
+    def test_invalid_json(self):
+        response = self.client.post(self.check_in_url, data="invalid JSON", content_type="application/json")
+        self.assertEqual(response.status_code, 400)
+
+        response_data = json.loads(response.content)
+        self.assertIn("error", response_data)
+        self.assertEqual(response_data.get("error"), "Invalid JSON")
