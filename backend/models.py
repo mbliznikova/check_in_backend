18b5c660a17866c81b511dcf03e1410ec104dc87
Margarita Bliznikova <captainderteufel@gmail.com>
2025-10-07 16:12:07 -0700

    Add is_recurring field to ClassModel (#14)

--- backend/models.py
+++ backend/models.py
@@ -17,6 +17,7 @@ class ClassModel(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50, unique=True)
     duration_minutes = models.PositiveIntegerField(default=60)
+    is_recurring = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
