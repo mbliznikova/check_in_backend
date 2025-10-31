7d82277a13273fad0c9096f68f225b59e9ec8df0
Margarita Bliznikova <captainderteufel@gmail.com>
2025-10-30 17:17:38 -0700

    Implement calculate_available_occurrence_time_intervals() #19

--- backend/urls.py
+++ backend/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path("payment_summary/", views.payment_summary, name="payment_summary"),
     path("schedules/", views.schedules, name="schedules"),
     path("schedules/<int:schedule_id>/delete/", views.delete_schedule, name="delete_schedule"),
-    path("available_time_slots", views.available_time_slots, name="available_time_slots"),
-    path("available_occurrence_time", views.available_occurrence_time, name="available_occurrence_time"),
+    path("available_time_slots/", views.available_time_slots, name="available_time_slots"),
+    path("available_occurrence_time/", views.available_occurrence_time, name="available_occurrence_time"),
 ]
