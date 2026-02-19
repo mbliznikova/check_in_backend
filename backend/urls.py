43f6c9a5cdedc686cdb862e991b68ae01a288121
Margarita Bliznikova <captainderteufel@gmail.com>
2026-02-18 16:47:38 -0800

    Removed duplicate url pattern #34

--- backend/urls.py
+++ backend/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.check_in, name="check_in"),
     path("check_in/", views.check_in, name="check_in"),
     path("confirm/", views.confirm, name="confirm"),
     path("attendances/", views.attendance_list, name="attendances"),
