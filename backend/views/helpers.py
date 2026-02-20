56d23cbc74808bbec2a79e364672b73de3e26736
Margarita Bliznikova <captainderteufel@gmail.com>
2026-02-19 19:33:52 -0800

    Use constants instead of hardcoded values #34

--- backend/views/helpers.py
+++ backend/views/helpers.py
@@ -1,6 +1,14 @@
 from django.http import JsonResponse
 
 
+# Default configuration constants
+DEFAULT_CLASS_NAME = "No name class"
+DEFAULT_CLASS_DURATION_MINUTES = 60
+DEFAULT_DAY_START_TIME = "08:00"
+DEFAULT_DAY_END_TIME = "21:00"
+DEFAULT_TIME_SLOT_STEP_MINUTES = 30
+
+
 def make_error_json_response(error_message, status_code):
     return JsonResponse({"error": error_message}, status=status_code)
 
